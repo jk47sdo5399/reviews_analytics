0cd77c217cdf88a819307883ee791a24c39502e0
sndz <506027572@qq.com>
2019-11-25 14:37:15 +0800

    计算平均留言的长度

--- reviews_analysis.py
+++ reviews_analysis.py
@@ -1,6 +1,17 @@
 data = []
+count = 0
 with open("053 reviews.txt",'r') as f:
     for line in f:
         data.append(line)
-print (len(data))
+        count += 1
+        if count % 1000 == 0:
+            print (len(data))
+# print (len(data))
+
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+average_len = sum_len/len(data)
+print(average_len)
+
 
